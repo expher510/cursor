2da76e7eb901ee0d73e7c5fef9ed89967f6fef61
ali saad <alisaadg9@gmail.com>
2026-01-07 09:21:22 +0000

    كبر الشعار  (_for element <LinkComponent>_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -3,8 +3,8 @@ import Link from 'next/link';
 
 export function Logo() {
   return (
-    <Link href="/" className="flex items-center gap-2 text-xl font-bold font-headline text-primary px-2">
-      <BookHeart className="h-6 w-6" />
+    <Link href="/" className="flex items-center gap-3 text-3xl font-bold font-headline text-primary px-2">
+      <BookHeart className="h-10 w-10" />
       <span>LinguaStream</span>
     </Link>
   );
