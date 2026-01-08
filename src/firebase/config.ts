35f2c9bdc8e6a19179035e418ce0fc1615933e91
ali saad <alisaadg9@gmail.com>
2026-01-08 06:41:10 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAODF58Q_EkPatukZdO71dU9L6wjnoq

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-6717952309-ee17b",
-  "appId": "1:883954175780:web:5284756039228db69e2db1",
-  "apiKey": "AIzaSyBzmwM7rkJMqyUmMasqgsFEnzGN1QO-1wo",
-  "authDomain": "studio-6717952309-ee17b.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "883954175780"
+  apiKey: "AIzaSyAODF58Q_EkPatukZdO71dU9L6wjnoqYMw",
+  authDomain: "studio-6717952309-ee17b.firebaseapp.com",
+  projectId: "studio-6717952309-ee17b",
+  storageBucket: "studio-6717952309-ee17b.appspot.com",
+  messagingSenderId: "883954175780",
+  appId: "1:883954175780:web:5284756039228db69e2db1"
 };
