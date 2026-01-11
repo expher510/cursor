37b7f4ed0772f5a3646a63d40b519084e2269ff9
ali saad <alisaadg9@gmail.com>
2026-01-11 19:00:32 +0000

    هو ايه الي حصل مش عارف ادوس علي الفديو

--- src/app/(app)/reading/page.tsx
+++ src/app/(app)/reading/page.tsx
@@ -194,8 +194,7 @@ function PageWithProvider() {
 
                     {!isLoading && videoData && (
                          <div className="group fixed right-4 bottom-8 z-50 h-40 w-40 flex items-center justify-center">
-                            
-                            <div className="absolute inset-0 flex items-center justify-center opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 transition-all duration-300">
+                            <div className="pointer-events-none absolute inset-0 flex items-center justify-center opacity-0 scale-0 group-hover:opacity-100 group-hover:scale-100 transition-all duration-300">
                                 <CircularProgressControl 
                                     progress={played * 100}
                                     onSeek={handleSeek}
